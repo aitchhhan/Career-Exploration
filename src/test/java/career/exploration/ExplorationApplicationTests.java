a558df27fa63341f27cf0941f865039128eecdbd
aitchhhan <aitchhhan@gmail.com>
2025-09-15 20:22:39 +0900

    Setting up...

---
+++ src/test/java/career/exploration/ExplorationApplicationTests.java
@@ -0,0 +1,13 @@
+package career.exploration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
